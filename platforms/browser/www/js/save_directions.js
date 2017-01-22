bd71bdfc884dc041eb476a62a10b0490305fbf1d
Emilie-Purr <emiliedannenberg@purrdigital.com>
2017-01-22 13:39:17 +0000

    get out data you need

--- platforms/browser/www/js/save_directions.js
+++ platforms/browser/www/js/save_directions.js
@@ -5,9 +5,13 @@ module.exports = function(directionObject) {
 
 	for (var i= 0; i< steps.length; i++) {
 
+		var currentStep=steps[i]; 
 
-
-
+		var direction=currentStep['direction']; 
+		var distance=currentStep['distance']; 
+		var instruction=currentStep['maneuver']['instruction']; 
+		var type=currentStep['maneuver']['type']; 
+		var coordinates=currentStep['maneuver']['location']['coordinates']; 
 	}
 
 
